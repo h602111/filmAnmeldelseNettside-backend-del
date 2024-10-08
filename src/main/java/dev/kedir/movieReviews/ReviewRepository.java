83451a434cb5988e711f1602d519739802a53d0f
h602111 <kedirdink@hotmail.com>
2024-10-09 00:39:02 +0200

    Ferdig

---
+++ src/main/java/dev/kedir/movieReviews/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.kedir.movieReviews;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
---
+++ src/test/java/dev/kedir/movieReviews/MovieReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kedir.movieReviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
